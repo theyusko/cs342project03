f5a56ce1021695e29155e66e9430e1e4fe24a1c0
theyusko <ecemilgun97@gmail.com>
2018-04-30 17:48:11 +0300

    added top level page table info, grouped code into functions

---
+++ 6 - pagetable entry/cmake-build-debug/CMakeFiles/5___stack_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/5___stack_test.dir/osp3.c.o"
+  "CMakeFiles/5___stack_test.dir/test.c.o"
+  "5___stack_test.pdb"
+  "5___stack_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/5___stack_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 6 - pagetable entry/cmake-build-debug/CMakeFiles/5___stack_test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 5___stack_test.
+# This may be replaced when dependencies are built.
